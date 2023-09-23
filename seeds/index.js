c612ba32b0bce316769e68de9184693898619f7e
Aditya122003 <mycoding2025@gmail.com>
2023-09-23 23:11:39 +0530

    Setting basic routes

--- seeds/index.js
+++ seeds/index.js
@@ -3,15 +3,10 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
+  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp'); //INSIDE FUNCTION WE CONNECT THE DATABASE
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
@@ -20,7 +15,7 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
-    }
+    }
 }
 
 seedDB()
