89a23d10166bee993d5ba49e753e8d3c389db595
Aditya122003 <mycoding2025@gmail.com>
2023-10-18 12:16:48 +0530

    Review validation

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const path = require("path")
 const mongoose = require('mongoose');
 const ejsMate = require("ejs-mate")
 const Joi = require('joi');
-const { campgroundSchema } = require('./schemas.js')
+const { campgroundSchema,reviewSchema } = require('./schemas.js')
 const catchAsync = require("./utils/catchAsync")
 const ExpressError = require("./utils/ExpressError")
 const methodOverride = require('method-override')
+const Review=require("./models/review")
 
 const Campground = require("./models/campground")
 main().catch(err => console.log(err));
@@ -34,6 +35,18 @@ const validateCampground = (req, res, next) => {
         next();
     }
 }
+
+const validateReview= (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else {
+        next();
+    }
+}
+
 app.get("/", (req, res) => {
     res.render("home")
 
@@ -75,6 +88,16 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id)
     res.redirect("/campgrounds")
 
+}))
+
+app.post("/campground/:id/reviews",validateReview, catchAsync(async (req, res) =>
+{
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.review.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all("*", (req, res, next) => {
--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,13 @@ const CampgroundSchema = new Schema({
     image:String,
     price:Number,
     description:String,
-    location:String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
 })
 module.exports = mongoose.model("Campground", CampgroundSchema);
 // // mongoose.model("Campground", CampgroundSchema) creates a Mongoose model named "Campground" 
