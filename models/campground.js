645631e7b2d40ac7447f8fbcf7c4d2d552260ad1
Aditya122003 <mycoding2025@gmail.com>
2023-11-05 11:52:19 +0530

    Updaing review form

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
+const Review = require('./review')
 const Schema = mongoose.Schema;
+
     
 const CampgroundSchema = new Schema({
     title: String,
@@ -13,6 +15,15 @@ const CampgroundSchema = new Schema({
             ref:'Review'
         }
     ]
+})
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 module.exports = mongoose.model("Campground", CampgroundSchema);
 // // mongoose.model("Campground", CampgroundSchema) creates a Mongoose model named "Campground" 
